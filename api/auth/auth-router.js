ceb3da3f8ded8b546803f1891d80898bdb2e0865
Fabian-Gorka <fabian-gorka@lambdastudents.com>
2021-05-24 22:31:58 -0500

    all edits

---
+++ api/auth/auth-router.js
@@ -0,0 +1,9 @@
+const buildToken = require("../utils/token-builder")
+
+const router = require("express").Router()
+
+//! add middleware when ready const {} = require("./")
+
+const bcrypt = require("bcryptjs")
+
+const 
